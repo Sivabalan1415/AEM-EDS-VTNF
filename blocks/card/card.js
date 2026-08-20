a01b7fbadde5e38032f451b53184ba77f87aa0f5
Sivabalan M <m.sivabalan20.4@gmail.com>
2026-08-20 22:45:50 +0530

    third

--- blocks/card/card.js
+++ blocks/card/card.js
@@ -1,20 +1,16 @@
 export default function decorate(block) {
   const cards = [...block.children];
   cards.forEach((card) => {
-    card.classList.add("medical-card");
-    const title = card.querySelector("h2");
-    console.log(title);
-    title?.classList.add("medical-card-title");
-    const pretitle = card.querySelector("h3,h4,h5,h6");
-    console.log(pretitle);
-    pretitle?.classList.add("medical-card-pretitle");
-    const image = card.querySelector("p:has(img)");
-    console.log(image);
-    image?.classList.add("medical-card-image");
-    const imageImg = image?.querySelector("img");
-    imageImg?.classList.add("medical-card-image-img");
-    const description = card.querySelector("p:not(:has(img))");
-    console.log(description);
-    description?.classList.add("medical-card-description");
+    card.classList.add('medical-card');
+    const title = card.querySelector('h2');
+    title?.classList.add('medical-card-title');
+    const pretitle = card.querySelector('h3,h4,h5,h6');
+    pretitle?.classList.add('medical-card-pretitle');
+    const image = card.querySelector('p:has(img)');
+    image?.classList.add('medical-card-image');
+    const imageImg = image?.querySelector('img');
+    imageImg?.classList.add('medical-card-image-img');
+    const description = card.querySelector('p:not(:has(img))');
+    description?.classList.add('medical-card-description');
   });
 }
--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,43 +1,34 @@
 export default function decorate(block) {
-    
   const firstblock = block.children[0];
-  console.log(firstblock);
-  firstblock.classList.add("custom-teaser");
-  const image = firstblock.querySelector("div:has(img)");
+  firstblock.classList.add('custom-teaser');
+  const image = firstblock.querySelector('div:has(img)');
   if (image) {
-    console.log(image);
-    image.classList.add("custom-teaser-image");
+    image.classList.add('custom-teaser-image');
   }
-  const content = firstblock.querySelector("div:not(:has(img))");
+  const content = firstblock.querySelector('div:not(:has(img))');
   if (content) {
-    console.log(content);
-    content.classList.add("custom-teaser-content");
+    content.classList.add('custom-teaser-content');
   }
-  const title = firstblock.querySelector("h1");
+  const title = firstblock.querySelector('h1');
   if (title) {
-    console.log(title);
-    title.classList.add("custom-teaser-title");
+    title.classList.add('custom-teaser-title');
   }
-  const pretitle = firstblock.querySelector("h2,h3,h4,h5,h6");
+  const pretitle = firstblock.querySelector('h2,h3,h4,h5,h6');
   if (pretitle) {
-    console.log(pretitle);
-    pretitle.classList.add("custom-teaser-pretitle");
+    pretitle.classList.add('custom-teaser-pretitle');
   }
-  const ul = firstblock.querySelector("ul");
+  const ul = firstblock.querySelector('ul');
   if (ul) {
-    console.log(ul);
-    ul.classList.add("custom-teaser-listgroup");
+    ul.classList.add('custom-teaser-listgroup');
   }
-  const li = firstblock.querySelectorAll("li");
+  const li = firstblock.querySelectorAll('li');
   if (li) {
-    console.log(li);
     li.forEach((item) => {
-      item.classList.add("custom-teaser-listitem");
+      item.classList.add('custom-teaser-listitem');
     });
   }
-  const description = firstblock.querySelector("p");
+  const description = firstblock.querySelector('p');
   if (description) {
-    console.log(description);
-    description.classList.add("custom-teaser-description");
+    description.classList.add('custom-teaser-description');
   }
 }
